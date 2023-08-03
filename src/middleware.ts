ffe647ca57741119c6e6e0bac1d9eec2b63159ea
MrZhouZh <17600330793@sina.cn>
2023-08-03 12:06:06 +0800

    fix: cors origin

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
 import { NextResponse } from "next/server"
 
 const allowOrigins = process.env.NODE_ENV === 'production'
-  ? ['https://www.yoursite.com', `${process.env.API_URL}`]
+  ? [
+      `https://next-restful-eizniie2y-mrzhouzh.vercel.app`,
+      `https://next-restful-api-mrzhouzh.vercel.app`,
+      `https://next-restful-api-git-main-mrzhouzh.vercel.app`,
+      `https://next-restful-api.vercel.app`,
+      `${process.env.API_URL}`
+    ]
   : [`${process.env.API_URL}`]
 
 export function middleware(req: Request) {
@@ -23,7 +29,20 @@ export function middleware(req: Request) {
   console.log({ method: req.method });
   console.log({ url: req.url });
 
-  return NextResponse.next()
+  const res = NextResponse.next()
+  // add the CORS headers to the response
+  if (origin && allowOrigins.includes(origin)) {
+    res.headers.append('Access-Control-Allow-Origin', origin);
+    // res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  }
+  res.headers.append('Access-Control-Allow-Credentials', "true")
+  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  res.headers.append(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
+  return res
 }
 
 export const config = {
