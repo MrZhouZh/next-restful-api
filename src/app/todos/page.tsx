348140ab0e0ac417e09e762d9fe1800ad3ff73d6
MrZhouZh <17600330793@sina.cn>
2023-08-03 10:22:32 +0800

    fix: build production

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,6 +1,9 @@
 import AddTodo from "~/app/components/AddTodo";
 import TodoList from "~/app/components/TodoList";
 
+export const runtime = "edge"
+// export const dynamic = "force-dynamic"
+
 export default function Todos() {
   return (
     <>
